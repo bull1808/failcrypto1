5075485c2ecb640b61883e07f8a7a7be507e1286
Bualt Tarkhanov <tarkhan1808@gmail.com>
2024-02-23 02:49:22 +0300

    init commit

---
+++ src/main/java/ru/javarush/tarkhanov/cryptoanalyzer/Runner.java
@@ -0,0 +1,10 @@
+package ru.javarush.tarkhanov.cryptoanalyzer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Runner {
+    public static void main(String[] args) {
+
+
+        }
+    }
